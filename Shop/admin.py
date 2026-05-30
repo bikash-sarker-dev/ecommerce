8f20828d8747d1e836ebef585d43f71922c3bf81
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-05-30 10:21:02 +0600

    the customer register admin

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from . models import (Customer, Product, Card, OrderPlaced)
+
 # Register your models here.
+@admin.register(Customer)
+class CustomerModalAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'name', 'division','district','thana', 'villorroad', 'zipCode']
--- Shop/models.py
+++ Shop/models.py
@@ -47,7 +47,7 @@ class Product(models.Model):
     
 
 
-class card(models.Model):
+class Card(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
