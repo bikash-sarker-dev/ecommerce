e7fe8159e39c01c66d43346b36123e4070cc0640
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-05-30 10:40:47 +0600

    the add min register data base file cutomer, prodect, card, orderplaced

--- Shop/admin.py
+++ Shop/admin.py
@@ -4,5 +4,22 @@ from . models import (Customer, Product, Card, OrderPlaced)
 
 # Register your models here.
 @admin.register(Customer)
-class CustomerModalAdmin(admin.ModelAdmin):
+class CustomerModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'name', 'division','district','thana', 'villorroad', 'zipCode']
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'selling_price', 'discounted_price','description','brand', 'category', 'product_image']
+
+
+
+@admin.register(Card)
+class CardModelAdmin(admin.ModelAdmin):
+     list_display = ['id', 'user', 'product', 'quantity']
+
+
+
+@admin.register(OrderPlaced)
+class OrderPacedModelAdmin(admin.ModelAdmin):
+     list_display = ['id', 'user', 'Customer', 'product','quantity','quantity', 'status']
