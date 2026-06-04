0991fc894895446b1c5c9622bbf50df846b5cdba
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-06-04 12:09:19 +0600

    the logout fucntiona done

--- Shop/urls.py
+++ Shop/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
 
     path('registration/', views.customerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page = 'login') , name="logout"),
+
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
