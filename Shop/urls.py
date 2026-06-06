6ac549f1bde8f81104935a624409ffefd856aa46
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-06-06 15:25:03 +0600

    the forgetpassword done

--- Shop/urls.py
+++ Shop/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name="Shop/password_reset.html", form_class=PasswordResetForm), name="password_reset"),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name = 'Shop/password_reset_done.html'), name="password_reset_done"),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='Shop/password_reset_confirm.html', form_class=MySetPasswordForm ), name='password_reset_confirm'),
- 
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="Shop/password_reset_complete.html"), name='password_reset_complete'),
 
 
     path('lehenga/', views.lehenga, name='lehenga'),
