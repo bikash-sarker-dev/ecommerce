b676a68ab1d6d9903dae04e2b36287b658ebf565
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-07-24 08:55:19 +0600

    the login done and refresh token create done

--- Shop/api_urls.py
+++ Shop/api_urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . api_views import index, Student, ResgisterView
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
 
     path('index/', index),
     path('student/', Student.as_view()),
     path('register/', ResgisterView.as_view(), name='api_register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+   
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
    
 ]
 
